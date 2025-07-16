3d7114376e3115f28f08fac9b9f750e2ffdb7e0b
James Vasile <james@jamesvasile.com>
2025-07-16 06:09:13 -0400

    Conform to convention

--- docker/test/tests/static-and-api-servers-up.test.js
+++ docker/test/tests/static-and-api-servers-up.test.js
@@ -4,12 +4,12 @@ describe('NEEMS React App Tests', () => {
   });
 
   // Test that we can load the home page, render the REACT, show the login page, and find the NEEMS EMS text.
-  it('load the home page', async () => {
+  it('should load the home page', async () => {
     const content = await page.content();
     expect(content).toMatch('NEEMS EMS');
   });
 
-  it('hit API endpoints', async () => {
+  it('should hit API endpoints', async () => {
     const response = await page.evaluate(async () => {
       const res = await fetch('/api/1/status');
       return await res.json();
