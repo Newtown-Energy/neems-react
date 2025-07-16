6c90b04d41e587161a462e600392e410b2d2c083
James Vasile <james@jamesvasile.com>
2025-07-15 20:48:51 -0400

    Test fiddling
     * Remove containers after test is done
     * Don't print nginx logs. Save them to disk instead.
     * Minor tweaks

--- docker/test/scripts/run-tests.sh
+++ docker/test/scripts/run-tests.sh
@@ -2,6 +2,7 @@
 
 ### THIS GETS RUN FROM INSIDE THE CONTAINER.  RUN THE TESTS WITH DOSH TEST
 
+
 # Wait for server using wget
 sleep 0.3
 while ! curl -sS http://nginx; do
@@ -10,12 +11,15 @@ done
 
 echo "Server is running and serving static pages."
 
-while ! curl -sS http://nginx/api/1/status; do
-   sleep 0.1
-done
+echo "nginx logs are in the react repo from where you're running 'dosh test'. Look in 'docker/test/nginx/logs'."
 
-echo
+wget --spider http://nginx/api/1/status || {
+  echo "API server is not responding. Exiting."
+  exit 1
+}
+
 echo "API Server is up at ${API_TARGET}"
+echo
 
 # Run tests with correct environment
 xvfb-run -a npx jest --config ./tests/jest.config.js
--- docker/test/tests/static-and-api-servers-up.test.js
+++ docker/test/tests/static-and-api-servers-up.test.js
@@ -4,12 +4,12 @@ describe('NEEMS React App Tests', () => {
   });
 
   // Test that we can load the home page, render the REACT, show the login page, and find the NEEMS EMS text.
-  it('should load the home page', async () => {
+  it('load the home page', async () => {
     const content = await page.content();
     expect(content).toMatch('NEEMS EMS');
   });
 
-  it('should hit API endpoints', async () => {
+  it('hit API endpoints', async () => {
     const response = await page.evaluate(async () => {
       const res = await fetch('/api/1/status');
       return await res.json();
